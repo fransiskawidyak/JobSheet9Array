efe2243961513da838d1ead5e98475dd6e5c246d
fransiskawidyak <fransiskawidyak17@gmail.com>
2023-10-12 14:52:01 +0700

    perulangam 1 percobaan 2 modifikasi 3

--- Documents/Praktikum Daspro/JobSheet7/WhileGaji08.java
+++ Documents/Praktikum Daspro/JobSheet7/WhileGaji08.java
@@ -1,38 +1,44 @@
 import java.util.Scanner;
+
 public class WhileGaji08 {
-    public static void main(String[]args){
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
         int jumlahKaryawan;
         int jumlahJamLembur;
-        double gajiLembur=0;
-        double totalGajiLembur=0;
+        double gajiLembur = 0;
+        double totalGajiLembur = 0;
         String jabatan;
 
-    System.out.print("Masukkan jumlah karyawan: ");
-    jumlahKaryawan = scan.nextInt();
+        System.out.print("Masukkan jumlah karyawan: ");
+        jumlahKaryawan = scan.nextInt();
 
-    int i = 0;
+        int i = 0;
 
-    while (i < jumlahKaryawan){
-        System.out.println(" Pilihan jabatan - Direktur,Manajer,Karyawan");
-        System.out.print("Masukkan jabatan karyawan ke- " + (i+1) + ":");
-        jabatan = scan.next();
-        System.out.print("Masukkan jumlah jam lembur: ");
-        jumlahJamLembur = scan.nextInt();
-        i++;
-        
-        if (jabatan.equalsIgnoreCase("direktur")){
-            continue;
-        } else if (jabatan.equalsIgnoreCase("manajer")){
-            gajiLembur = jumlahJamLembur * 100000;
+        while (i < jumlahKaryawan) {
+            System.out.println(" Pilihan jabatan - Direktur,Manajer,Karyawan");
+            System.out.println("Masukkan jabatan karyawan ke- " + (i + 1) + ":");
+            jabatan = scan.next();
+            System.out.print("Masukkan jumlah jam lembur: ");
+            jumlahJamLembur = scan.nextInt();
 
+            if (jabatan.equalsIgnoreCase("direktur")) {
+                continue;
+            } else if (jabatan.equalsIgnoreCase("manajer")) {
+                gajiLembur = jumlahJamLembur * 100000;
+            } else if (jabatan.equalsIgnoreCase("karyawan")) {
+                gajiLembur = jumlahJamLembur * 75000;
+            } else {
+                System.out.println("Jabatan tidak valid");
+            }
+            totalGajiLembur += gajiLembur;
+            i++;
         }
-          else if (jabatan.equalsIgnoreCase("karyawan")){
-            gajiLembur = jumlahJamLembur * 75000;
 
-          }
-          totalGajiLembur += gajiLembur;
-        System.out.println("Total gaji lembur:" + totalGajiLembur);
-    }
+        if (i == 0) {
+            System.out.println("Tidak ada karyawan");
+        } else {
+            System.out.println("Total gaji lembur: " + totalGajiLembur);
+        }
     }
 }
