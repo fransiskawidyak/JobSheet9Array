9dd432b28ab81111e236070021764717248e6e72
fransiskawidyak <fransiskawidyak17@gmail.com>
2023-10-12 15:45:50 +0700

    perulangan 1 percobaan 1 modifikasi 2

--- Documents/Praktikum Daspro/JobSheet7/WhileKelipatan08.java
+++ Documents/Praktikum Daspro/JobSheet7/WhileKelipatan08.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class WhileKelipatanNoAbsen {
+public class WhileKelipatan08 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
@@ -10,29 +10,22 @@ public class WhileKelipatanNoAbsen {
         int total = 0;
         float rataRata = 0;
 
-        int i;
-
         System.out.print("Masukkan bilangan kelipatan (1-9):");
         kelipatan = scan.nextInt();
 
-        if (kelipatan < 1 || kelipatan > 9) {
-            System.out.println("Nilai kelipatan harus lebih besar dari 1 dan kurang dari atau sama dengan 9");
-            return;
-        }
-
-        i = 5;
-
+        int i = 1;
         while (i <= 50) {
             if (i % kelipatan == 0) {
-                jumlah++;
                 total += i;
-                rataRata = total / jumlah;
+                counter++;
+
+                rataRata = total / counter;
+
+                System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
+                System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, total);
+                System.out.printf("Rata-rata bilangan kelipatan %d dari 1 sampai 50 adalah %.2f\n", kelipatan, rataRata);
             }
             i++;
         }
-
-        System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, jumlah);
-        System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, total);
-        System.out.printf("Rata-rata bilangan kelipatan %d dari 1 sampai 50 adalah %.2f\n", kelipatan, rataRata);
     }
 }
