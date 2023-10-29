b18a41c7b7b040619a807cded27e7ade7b241ba5
fransiskawidyak <fransiskawidyak17@gmail.com>
2023-10-29 21:25:53 +0700

    percobaan 2 dimodif

--- Documents/Praktikum Daspro/JobSheet9/ArrayNilai08.java
+++ Documents/Praktikum Daspro/JobSheet9/ArrayNilai08.java
@@ -5,14 +5,22 @@ public class ArrayNilai08 {
         Scanner sc = new Scanner(System.in);
 
         int[] nilaiAkhir = new int[10];
+        String[] status = new String[10];
 
         for (int i = 0; i < 10; i++) {
             System.out.print("Masukkan nilai akhir ke-" + i + " : ");
             nilaiAkhir[i] = sc.nextInt();
+
+            // Menentukan status kelulusan
+            if (nilaiAkhir[i] >= 75) {
+                status[i] = "Lulus";
+            } else {
+                status[i] = "Tidak Lulus";
+            }
         }
 
         for (int i = 0; i < 10; i++) {
-            System.out.println("Nilai akhir ke-" + i + " adalah " + nilaiAkhir[i]);
+            System.out.println("Nilai akhir ke-" + i + " adalah " + nilaiAkhir[i] + " (" + status[i] + ")");
         }
     }
 }
