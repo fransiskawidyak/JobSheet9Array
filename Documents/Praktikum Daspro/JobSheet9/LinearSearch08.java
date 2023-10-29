0b2b768ed35881ca0bb0f73276a979c0a3375a21
fransiskawidyak <fransiskawidyak17@gmail.com>
2023-10-29 22:13:50 +0700

    percobaan 4 dimodif

--- Documents/Praktikum Daspro/JobSheet9/LinearSearch08.java
+++ Documents/Praktikum Daspro/JobSheet9/LinearSearch08.java
@@ -4,13 +4,20 @@ public class LinearSearch08 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        System.out.print("Masukkan jumlah elemen array: ");
+        int banyakElemen = sc.nextInt();
+
+        int[] arrayInt = new int[banyakElemen];
+        for (int i = 0; i < banyakElemen; i++) {
+            System.out.print("Masukkan elemen array ke-" + (i + 1) + ": ");
+            arrayInt[i] = sc.nextInt();
+        }
+
         System.out.print("Masukkan key yang ingin dicari: ");
         int key = sc.nextInt();
 
-        int[] arrayInt = {34, 18, 26, 48, 72, 20, 56, 63};
         int hasil = -1;
-
-        for (int i = 0; i < arrayInt.length; i++) {
+        for (int i = 0; i < banyakElemen; i++) {
             if (arrayInt[i] == key) {
                 hasil = i;
                 break;
