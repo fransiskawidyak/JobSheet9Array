7078975ca28467a49cddab9dc3f8dcc4d33dbe93
fransiskawidyak <fransiskawidyak17@gmail.com>
2023-10-29 21:50:15 +0700

    percobaan 3 dimodif

--- Documents/Praktikum Daspro/JobSheet9/ArrayRataNilai08.java
+++ Documents/Praktikum Daspro/JobSheet9/ArrayRataNilai08.java
@@ -4,18 +4,33 @@ public class ArrayRataNilai08 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int[] nilaiMhs = new int[10];
-        double total = 0;
-        double rata2;
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        int jumlahNilai = sc.nextInt();
+        int[] nilaiMhs = new int[jumlahNilai];
+        double totalLulus = 0;
+        double totalTidakLulus = 0;
+        int jumlahLulus = 0;
+        int jumlahTidakLulus = 0;
 
         for (int i = 0; i < nilaiMhs.length; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + " : ");
             nilaiMhs[i] = sc.nextInt();
-            total += nilaiMhs[i];
+
+            if (nilaiMhs[i] >= 70) {
+                totalLulus += nilaiMhs[i];
+                jumlahLulus++;
+            } else {
+                totalTidakLulus += nilaiMhs[i];
+                jumlahTidakLulus++;
+            }
         }
 
-        rata2 = total / nilaiMhs.length; 
+        double rata2Lulus = totalLulus / jumlahLulus;
+        double rata2TidakLulus = totalTidakLulus / jumlahTidakLulus;
 
-        System.out.println("Rata-rata nilai = " + rata2);
+        System.out.println("Rata-rata nilai lulus = " + rata2Lulus);
+        System.out.println("Rata-rata nilai tidak lulus = " + rata2TidakLulus);
+        System.out.println("Jumlah mahasiswa yang lulus: " + jumlahLulus);
+        System.out.println("Jumlah mahasiswa yang tidak lulus: " + jumlahTidakLulus);
     }
 }
