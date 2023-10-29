b9e4f6d2a4e706dc228d08bc7c553fbb76abe531
fransiskawidyak <fransiskawidyak17@gmail.com>
2023-10-29 20:45:26 +0700

    percobaan 1 sudah dimodif

--- Documents/Praktikum Daspro/JobSheet9/ArrayBilangan08.java
+++ Documents/Praktikum Daspro/JobSheet9/ArrayBilangan08.java
@@ -1,17 +1,10 @@
 import java.util.Scanner;
-public class ArrayBilangan08{
-    public static void main(String[] args){
-        int[] bil = new int[4];
-        bil[0] = 5;
-        bil[1] = 13;
-        bil[2] = -7;
-        bil[3] = 17;
+public class ArrayBilangan08 {
+    public static void main(String[] args) {
+        double[] bil = {5.0, 12867, 7.5, 2000000};
 
-        System.out.println(bil[0]);
-        System.out.println(bil[1]);
-        System.out.println(bil[2]);
-        System.out.println(bil[3]);
-
-
+        for (int i = 0; i <= 4; i++) {
+            System.out.println(bil[i]);
+        }
     }
 }
