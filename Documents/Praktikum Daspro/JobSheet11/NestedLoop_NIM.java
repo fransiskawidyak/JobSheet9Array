f35f65bc3e7b7e1a060190ded6e0f4c30ca8321b
fransiskawidyak <fransiskawidyak17@gmail.com>
2023-11-09 20:48:04 +0700

    percobaan Nestedloop'

--- Documents/Praktikum Daspro/JobSheet11/NestedLoop_NIM.java
+++ Documents/Praktikum Daspro/JobSheet11/NestedLoop_NIM.java
@@ -1,26 +1,34 @@
 import java.util.Scanner;
+
 public class NestedLoop_NIM {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[][] temps = new int[5][7];
-        int value = temps[2][1];
 
-        for (int i = 0; i< temps.length; i++) { 
-            System.out.println("Kota ke-" + i); 
-            for (int j = 0; j < temps[0].length; j++) { 
-                System.out.print("Hari ke-" + (j + 1) + ": "); 
-                temps[i][j] = sc.nextInt(); 
-                    } 
-                        System.out.println();
-                    }
+        for (int i = 0; i < temps.length; i++) {
+            System.out.println("Kota ke-" + i);
+            for (int j = 0; j < temps[0].length; j++) {
+                System.out.print("Hari ke-" + (j + 1) + ": ");
+                temps[i][j] = sc.nextInt();
+            }
+            System.out.println();
+        }
 
-                for (int i = 0; i < temps.length; i++) { 
-                    System.out.print("Kota ke-" + (i + 1) + ": "); 
-                    for (int j = 0; j < temps[0].length; j++) { 
-                            System.out.print(temps[i][j] + " "); 
-            } 
-            System.out.println(); 
-           }
-           
+        for (int i = 0; i < temps.length; i++) {
+            System.out.print("Kota ke-" + (i + 1) + ": ");
+            for (int temp : temps[i]) {
+                System.out.print(temp + " ");
+            }
+            System.out.println();
+        }
+
+        for (int i = 0; i < temps.length; i++) {
+            int total = 0;
+            for (int temp : temps[i]) {
+                total += temp;
+            }
+            double rataRata = (double) total / temps[i].length;
+            System.out.println("Rata-rata Kota ke-" + (i + 1) + ": " + rataRata);
+        }
     }
 }
